767363b07f623847c798a766d9a126da06d3601e
kwngnic <kwng.nic@gmail.com>
2015-08-15 11:35:34 -0400

    Typo fix

--- Folder_Creation_Copying.py
+++ Folder_Creation_Copying.py
@@ -18,8 +18,8 @@ if not os.path.exists(newpath):
 def copy_file(src):
 	shutil.copy(src, destination)
 
-copy_file("C:\Users\{0}\Music\CopyFile.txt").format(name)
-copy_file("C:\Users\{0}\Music\TestMP3.mp3").format(name)
-copy_file("C:\Users\{0}\Music\Blazo - Cult Classic Records Present- Friends and Family - 03 Busy Dreaming.mp3").format(name)
-copy_file(r"C:\Users\{0}\Music\13 Nutritious - Dash Cancel [Ultra Street Fighter IV].mp3").format(name)
+copy_file("C:\Users\{0}\Music\CopyFile.txt".format(name))
+copy_file("C:\Users\{0}\Music\TestMP3.mp3".format(name))
+copy_file("C:\Users\{0}\Music\Blazo - Cult Classic Records Present- Friends and Family - 03 Busy Dreaming.mp3".format(name))
+copy_file(r"C:\Users\{0}\Music\13 Nutritious - Dash Cancel [Ultra Street Fighter IV].mp3".format(name))
 
