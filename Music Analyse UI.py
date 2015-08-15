4c3055441120e8d502a2c7b1e30a8033b85dfb9e
Will-Lo <will-i-am_95@hotmail.com>
2015-08-15 13:56:50 -0400

    Modified UI
    grabs file location and closes popup

--- Music Analyse UI.py
+++ Music Analyse UI.py
@@ -2,8 +2,13 @@ import kivy
 
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
+from kivy.uix.floatlayout import FloatLayout
 from kivy.lang import Builder
 from kivy.uix.popup import Popup
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.properties import StringProperty, ListProperty, ObjectProperty, NumericProperty
+
 
 import os
 
@@ -13,39 +18,56 @@ Builder.load_string("""
 	auto_dismiss: False
 	title: 'Hello world'
 	FileChooserListView:
-		id: filechooser
-		on_selection: my_widget.selected(filechooser.selection)
+		on_selection: root.selected(self.selection)
+		
 
 <MyWidget>:
 	id: my_widget
-	Button:
-		text: "open"
-		on_release: my_widget.open(filechooser.path, filechooser.selection)
+	Label:
+		text: "Sort your music based on their mood"
+		font_size: 25
+		pos_hint: {'center_x': 0.5, 'center_y': 0.85}
+		size_hint: 0.6,0.4
+	
+	
 	Button:
 		text: "choose music file to analyse"
 		on_release: root.show_popup()
+		pos_hint: {'center_x': 0.5, 'center_y': 0.35}
+		size_hint: 0.6,0.2
+
+<HomeScreen>:
+	BoxLayout:
+		MyWidget
 """)
+#global variables containing where the file location is
+file_location = "" 
 
-class MyWidget(BoxLayout):
-	def open(self, path, filename):
-		with open(os.path.join(path, filename[0])) as f:
-			print f.read()
 
-	def selected(self, filename):
-		print "selected: %s" % filename[0]
+
+class MyWidget(FloatLayout):
 	
 	def show_popup(self):
 		p = PopupWindow()
 		p.open()
 
-
-		
-class PopupWindow(Popup):
+class HomeScreen(Screen):
 	pass
+box = FloatLayout()
+	
+class PopupWindow(Popup):
+	
+	def selected(self, filename):
+		print "selected: %s" % filename[0]
+		global file_location 
+		file_location = filename[0]
+		self.dismiss(force = True)
+		
 
 class MyApp(App):
 	def build(self):
-		return MyWidget()
+		return HomeScreen()
 
+		
 if __name__ == '__main__':
 	MyApp().run()
