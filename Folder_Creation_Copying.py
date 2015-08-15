c712f95daffa7838dfdb5b95030bc5fba587b009
kwngnic <kwng.nic@gmail.com>
2015-08-15 11:26:59 -0400

    Made finding user name variable

--- Folder_Creation_Copying.py
+++ Folder_Creation_Copying.py
@@ -5,7 +5,9 @@ import shutil
 #New folder to be created in python file directory
 newpath = r'Test'
 
-destination = "C:\Users\Colin\Documents\GitHub\Tech-Retreat-Demo\Test"
+#To be changed in Kivy
+name = os.listdir(r"C:\Users")[1]
+destination = "C:\Users\{0}\Documents\GitHub\Tech-Retreat-Demo\Test".format(name)
 
 #Creates new folder if not already existing
 if not os.path.exists(newpath):
@@ -18,3 +20,6 @@ def copy_file(src):
 
 copy_file("C:\Users\Colin\Music\CopyFile.txt")
 copy_file("C:\Users\Colin\Music\TestMP3.mp3")
+copy_file("C:\Users\Colin\Music\Blazo - Cult Classic Records Present- Friends and Family - 03 Busy Dreaming.mp3")
+copy_file(r"C:\Users\Colin\Music\13 Nutritious - Dash Cancel [Ultra Street Fighter IV].mp3")
+
