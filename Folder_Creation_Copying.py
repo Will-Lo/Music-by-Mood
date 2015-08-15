e68f842d0360eb5cad98b145457fa004f2f0763a
kwngnic <kwng.nic@gmail.com>
2015-08-15 12:44:26 -0400

    Functions and Sub Folder Creation

--- Folder_Creation_Copying.py
+++ Folder_Creation_Copying.py
@@ -10,16 +10,27 @@ name = os.listdir(r"C:\Users")[1]
 destination = "C:\Users\{0}\Documents\GitHub\Tech-Retreat-Demo\Test".format(name)
 
 #Creates new folder if not already existing
-if not os.path.exists(newpath):
-    os.mkdir(newpath)
-    print "Path Created"
+def mainFolder(new):
+    if not os.path.exists(new):
+        os.mkdir(new)
+        print "Path Created"
 
+#Creates sub folder        
+def sub_folder(mainF, sub):
+    if not os.path.exists("{0}/{1}".format(mainF, sub)):
+        os.mkdir("{0}/{1}".format(mainF, sub))
+        
 #Copies a given file
 def copy_file(src):
     shutil.copy(src, destination)
 
-copy_file("C:\Users\{0}\Music\CopyFile.txt".format(name))
-copy_file("C:\Users\{0}\Music\TestMP3.mp3".format(name))
-copy_file("C:\Users\{0}\Music\Blazo - Cult Classic Records Present- Friends and Family - 03 Busy Dreaming.mp3".format(name))
-copy_file(r"C:\Users\{0}\Music\13 Nutritious - Dash Cancel [Ultra Street Fighter IV].mp3".format(name))
+def main():     
+    
+    mainFolder(newpath)
+    sub_folder(newpath, "Playlist")
+    copy_file("C:\Users\{0}\Music\CopyFile.txt".format(name))
+    copy_file("C:\Users\{0}\Music\TestMP3.mp3".format(name))
+    copy_file("C:\Users\{0}\Music\Blazo - Cult Classic Records Present- Friends and Family - 03 Busy Dreaming.mp3".format(name))
+    copy_file(r"C:\Users\{0}\Music\13 Nutritious - Dash Cancel [Ultra Street Fighter IV].mp3".format(name))
 
+main()
